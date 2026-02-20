224e17f6e6041f6deac72ac3f600f7345778439c
Gemma Giliberti <7432.GILIBERTI@uniroma2.onmicrosoft.com>
2026-02-20 12:19:31 +0100

    Fix: new fix normlaizer()

--- src/optical_constant_plugin/normalizers/mynormalizer.py
+++ src/optical_constant_plugin/normalizers/mynormalizer.py
@@ -1,20 +1,18 @@
 from nomad.config.models.plugins import NormalizerEntryPoint
-from nomad.normalizing.normalizer import Normalizer
-
-
-class OpticalNormalizer(Normalizer):
-    def normalize(self, archive, logger):
-        # Stub: non fa nulla
-        return
-
 
 class OpticalNormalizerEntryPoint(NormalizerEntryPoint):
     name: str = "optical_normalizer"
     description: str = "Stub normalizer for optical constants."
 
     def load(self):
-        # DEVE ritornare un'istanza di Normalizer
-        return OpticalNormalizer()
+        # Import LAZY per evitare circular import durante config.load_plugins()
+        from nomad.normalizing.normalizer import Normalizer
 
+        class OpticalNormalizer(Normalizer):
+            def normalize(self, archive, logger):
+                # Stub: non fa nulla
+                return
+
+        return OpticalNormalizer()
 
 optical_normalizer = OpticalNormalizerEntryPoint()
